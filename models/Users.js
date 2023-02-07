8b0aaa8b1c0e3b1e74539f61ddc69656a3101cfa
Luca Barnabo <lucabarnabo@yahoo.com.ar>
2023-02-07 00:06:38 -0300

    Arreglar token

--- models/Users.js
+++ models/Users.js
@@ -1,15 +1,36 @@
 const mysql = require("mysql");
 /* const bcrypt = require("bcrypt"); */
-const db = require("../db");
+/* const db = require("../db"); */
 const { handleError, ErrorHandler } = require("../helpers/error");
 
-const getAll = () => {
-  console.log("into get all");
+const db = mysql.createPool({
+  hots: process.env.DB_HOST,
+  user: "root",
+  password: "root",
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+
+const getAll = async () => {
   return new Promise((resolve, reject) => {
-    db.query(`SELECT * FROM users WHERE disabled = NULL`, (err, rows) => {
+    db.query(`SELECT * FROM users`, (err, rows) => {
       if (err) reject(err);
       resolve(rows);
     });
+  }).catch((err) => {
+    throw new ErrorHandler(400, err.sqlMessage);
+  });
+};
+
+const getByName = async (nombreUsuario) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `SELECT * FROM users WHERE username = '${nombreUsuario}'`,
+      (err, rows) => {
+        if (err) reject(err);
+        resolve(rows);
+      }
+    );
   }).catch((err) => {
     throw new ErrorHandler(400, err.sqlMessage);
   });
@@ -34,7 +55,47 @@ const insert = ({ email, password, nombre, apellido, telefono, rol_id }) => {
   });
 };
 
+const login = async (username, password) => {
+  const user = await getByName(username);
+  if (!user) {
+    return false;
+  } else {
+    return await checkPassword(password, user[0].password);
+  }
+};
+
+const checkPassword = async (password, userPassword) => {
+  /* const equals = bcrypt.compareSync (password, userPassword); */
+
+  let token = async () => {
+    if (password === userPassword) {
+      res.status(200).json({
+        token: createToken(user),
+        id: user.id,
+        rol_id: user.rol_id,
+        nombre: user.nombre,
+        apellido: user.apellido,
+        message: "Inicio de sesión correcta",
+      });
+    } else {
+      res.status(401).json("Error, nombre o contraseña no encontrados");
+    }
+  };
+  console.log(token);
+  return await token;
+};
+
+const createToken = async (user) => {
+  let payload = {
+    userId: user.id,
+    createdAT: moment().unix(),
+    expiresAT: moment().add(1, "day").unix(),
+  };
+  return await jwt.encode(payload, process.env.TOKEN_KEY);
+};
+
 module.exports = {
   insert: insert,
   getAll: getAll,
+  login: login,
 };
